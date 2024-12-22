7162984994add1c53e19f32e8114931839d38754
freakila <150614295+mfnnx@users.noreply.github.com>
2024-12-22 23:01:07 +0300

    Create rectangle.py

---
+++ lab4/lab_python_oop/rectangle.py
@@ -0,0 +1,21 @@
+from lab_python_oop.figure import Figure
+from lab_python_oop.color import Color
+
+class Rectangle(Figure):
+    FIGURE_TYPE = "Прямоугольник"
+
+    def __init__(self, width, height, color):
+        self.width = width
+        self.height = height
+        self.color = Color(color)
+
+    def area(self):
+        return self.width * self.height
+
+    @classmethod
+    def name(cls):
+        return cls.FIGURE_TYPE
+
+    def __repr__(self):
+        return "Фигура: {}, цвет: {}, ширина: {}, высота: {}, площадь: {:.2f}".format(
+            self.name(), self.color.color, self.width, self.height, self.area()
